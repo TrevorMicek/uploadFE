68b96d4be7643363474a0a2555b6ef98c22b0f4e
Trevor Micek <webdevtrevor@gmail.com>
2026-09-10 02:13:30 -0600

    updated origin

--- src/components/FeatureSectionCols.jsx
+++ src/components/FeatureSectionCols.jsx
@@ -52,10 +52,13 @@ export default function FeatureSection() {
     formData.append("description", "A test file upload");
 
     try {
-      const response = await fetch("http://localhost:3000/upload", {
-        method: "POST",
-        body: formData, // The browser sets the Content-Type header automatically
-      });
+      const response = await fetch(
+        "https://uploadfe.webdevtrevor.workers.dev/",
+        {
+          method: "POST",
+          body: formData, // The browser sets the Content-Type header automatically
+        },
+      );
       GetPage("images")
         .then((res) => res.json())
         .then(
