5a1cc3e1d86cdd86c3bd33ff71905b908766f451
Trevor Micek <webdevtrevor@gmail.com>
2026-09-10 14:18:33 -0600

    update origin again, preflight

--- src/components/GetPage.jsx
+++ src/components/GetPage.jsx
@@ -9,7 +9,7 @@ const getUrl = (page, origin) => {
     }
   } else {
     if (origin) {
-      return `https://uploadfe.webdevtrevor.workers.dev/`;
+      return `https://uploadfe.webdevtrevor.workers.dev`;
     } else {
       return `https://upload-pn3q.onrender.com/data/${page}`;
     }
