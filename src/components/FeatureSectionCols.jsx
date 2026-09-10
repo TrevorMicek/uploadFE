2d8a083e142e1a3ee2af9fa6b84af470ccd143b0
Trevor Micek <webdevtrevor@gmail.com>
2026-09-10 14:36:31 -0600

    fixed upload url

--- src/components/FeatureSectionCols.jsx
+++ src/components/FeatureSectionCols.jsx
@@ -52,13 +52,10 @@ export default function FeatureSection() {
     formData.append("description", "A test file upload");
 
     try {
-      const response = await fetch(
-        "https://upload-pn3q.onrender.com/data/upload",
-        {
-          method: "POST",
-          body: formData, // The browser sets the Content-Type header automatically
-        },
-      );
+      const response = await fetch("https://upload-pn3q.onrender.com/upload", {
+        method: "POST",
+        body: formData, // The browser sets the Content-Type header automatically
+      });
       GetPage("images")
         .then((res) => res.json())
         .then(
